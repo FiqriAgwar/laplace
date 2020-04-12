554069d7b9843c7337e70331b1422bc7c4e0e751
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2020-04-12 15:13:54 +0700

    horizontal movement

---
+++ Hack Race/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float terminalForwardSpeed;
+    public float terminalBackwardSpeed;
+    public float acceleration;
+    public float decceleration;
+    public float friction;
+
+    private float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = 0f;
+        terminalBackwardSpeed = -terminalBackwardSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Accelerate") > 0){
+            currentSpeed += acceleration;
+            if(currentSpeed >= terminalForwardSpeed){
+                currentSpeed = terminalForwardSpeed;
+            }
+        }
+        else if(Input.GetAxisRaw("Accelerate") < 0){
+            currentSpeed -= decceleration;
+            if(currentSpeed <= terminalBackwardSpeed){
+                currentSpeed = terminalBackwardSpeed;
+            }
+        }
+        else{
+            currentSpeed -= friction;
+            if(currentSpeed <= 0){
+                currentSpeed = 0;
+            }
+        }
+
+        Debug.Log(currentSpeed);
+
+        transform.Translate(0, 0, currentSpeed * Time.deltaTime);
+        
+    }
+}
